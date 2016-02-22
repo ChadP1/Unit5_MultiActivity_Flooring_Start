b30b0d5a01717743aeba00c321864d3a685bc27b
ChadP1 <unopoop@gmail.com>
2016-02-21 22:48:08 -0600

    Finished project

--- app/src/main/java/edu/css/cis3334/unit5_multiactivity_flooring_start/MainActivity.java
+++ app/src/main/java/edu/css/cis3334/unit5_multiactivity_flooring_start/MainActivity.java
@@ -1,17 +1,28 @@
 package edu.css.cis3334.unit5_multiactivity_flooring_start;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class MainActivity extends ActionBarActivity {
+    public final static String EXTRA_MESSAGE = "The area is: ";
+    protected EditText width;
+    protected EditText length;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        width = (EditText)findViewById(R.id.etWidth);
+        length = (EditText)findViewById(R.id.etLength);
+
+
     }
 
 
@@ -36,4 +47,24 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+
+        int len = Integer.parseInt(length.getText().toString());
+        int wid = Integer.parseInt(width.getText().toString());
+
+        int sum = wid * len;
+
+        //String message = sum.getText().toString();
+
+        intent.putExtra(EXTRA_MESSAGE, sum);
+        startActivity(intent);
+
+
+
+    }
+
+
+
 }
